671aa9b76b64336a053f301705a29ff0857703ad
trpelis <trajevskij@yahoo.com>
2023-04-26 17:14:04 +0200

    .

---
+++ python/server2_responser.py
@@ -0,0 +1,19 @@
+import http.server
+import socketserver
+
+PORT = 9443
+
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/9443':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b'server2 responding')
+        else:
+            super().do_GET()
+
+with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+    print(f"serving at port {PORT}")
+    httpd.serve_forever()
+
